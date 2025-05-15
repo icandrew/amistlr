b88cd5b38513bcee2fc59df1747eac80c7493a32
Andrew Jake Bagsit <244024.JBagsit@gbs.local>
2025-05-16 07:24:36 +0800

    Update the images in timeline-demo component

--- components/timeline-demo.tsx
+++ components/timeline-demo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { Timeline } from "@/components/ui/timeline";
 
 export function TimelineDemo() {
@@ -11,33 +12,33 @@ export function TimelineDemo() {
             Built and launched Aceternity UI and Aceternity UI Pro from scratch
           </p>
           <div className="grid grid-cols-2 gap-4">
-            <img
-              src="https://assets.aceternity.com/templates/startup-1.webp"
-              alt="startup template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/templates/startup-2.webp"
-              alt="startup template"
+            <Image
+              src="/proj-2.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/templates/startup-3.webp"
-              alt="startup template"
+            <Image
+              src="/proj-3.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/templates/startup-4.webp"
-              alt="startup template"
+            <Image
+              src="/proj-4.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
           </div>
         </div>
@@ -56,33 +57,33 @@ export function TimelineDemo() {
             not. Here are some more example of beautiful designs I built.
           </p>
           <div className="grid grid-cols-2 gap-4">
-            <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
+            <Image
+              src="/proj-5.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
           </div>
         </div>
@@ -113,33 +114,33 @@ export function TimelineDemo() {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4">
-            <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
-            <img
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
+            <Image
+              src="/proj-1.webp"
+              alt="project 1"
               width={500}
               height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+              className="h-20 w-full rounded-lg object-cover bg-neutral-200 md:h-44 lg:h-60 transition-transform duration-200 hover:scale-105 hover:shadow-lg"
             />
           </div>
         </div>
